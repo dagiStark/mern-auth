45e659dd58f090ffde1ee20b70ba9acdf0fefd6a
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-15 15:59:29 +0300

    update index.js server endpoint

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import { json } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 import { connectDB } from "./db/connectDB.js";
-// import { signup, login, logout } from "./controllers/auth.js";
 
-
-import authRoute from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("MERN Auth");
-});
+app.use("/api/auth", authRoutes);
 
-// app.post("/signup", signup);
-// app.post("/login", login);
-// app.post("/logout", logout);
-app.use("/api/auth", authRoute);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
-connectDB();
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port: ", PORT);
 });
